1c85807d0892d924b74080e561224865558e1948
dannytranlx <danny@dannytran.ca>
2014-04-30 18:53:15 -0400

    Add regex for raw station data

--- application.py
+++ application.py
@@ -1,22 +1,35 @@
 from flask import Flask
 from flask import render_template
-from bs4 import BeautifulSoup
 import urllib2
+import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template('index.html', data = fetch_data())
+	return render_template("index.html")
 
 @app.route("/api")
 def api():
 	data = fetch_data()
-	return data
+	return ','.join(data)
 
 def fetch_data():
-	
-	return 'Aucun station trouv&eacute;e'
+	url_bixi = "http://montreal.bixi.com/maps/statajax"
+
+	try:
+		webparser = urllib2.urlopen(url_bixi).read()
+	except:
+		return "Error: Cannot read the feed"
+
+	if webparser:
+		data = re.findall("var station\s=\s\\{(.*?)\\}", webparser)
+		
+
+	if data:
+		return data
+
+	return "No station found"
 
 if __name__ == "__main__":
 	app.debug = True
